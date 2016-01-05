ce1e01fabf9040b8102c1d829692c4a64c29d335
yalab <rudeboyjet@gmail.com>
2016-01-05 16:08:15 +0900

    First commit.

---
+++ Touchable.cpp
@@ -0,0 +1,59 @@
+//
+//  Touchable.cpp
+//  angel
+//
+//  Created by Atsushi Yoshida on 2015/12/20.
+//
+//
+
+#include "Touchable.h"
+
+
+const std::string Touchable::LONG_TOUCH_KEY = "longTouch";
+const float Touchable::LONG_TOUCH_SEC = 1.0f;
+void Touchable::onLongTouch(Widget* widget, const std::function<void(Ref* ref)>& action)
+{
+    widget->setTouchEnabled(true);
+    widget->addTouchEventListener([&, action](Ref* ref, Widget::TouchEventType eventType){
+        auto widget = static_cast<Widget*>(ref);
+        if(eventType == Widget::TouchEventType::BEGAN){
+            widget->schedule([&, ref, action](float dt){
+                _dt += dt;
+                if(_dt > Touchable::LONG_TOUCH_SEC){
+                    _dt = 0;
+                    action(ref);
+                    static_cast<Widget*>(ref)->unschedule(Touchable::LONG_TOUCH_KEY);
+                }
+            }, Touchable::LONG_TOUCH_KEY);
+        }
+        if(eventType == Widget::TouchEventType::ENDED){
+            static_cast<Widget*>(ref)->unschedule(Touchable::LONG_TOUCH_KEY);
+            _dt = 0;
+        }
+    });
+}
+
+void Touchable::onTouch(Widget* widget,  const std::function<void(Ref* ref)>& action)
+{
+    widget->setTouchEnabled(true);
+    widget->addTouchEventListener([widget, action](Ref* ref, Widget::TouchEventType eventType){
+        if(eventType == Widget::TouchEventType::ENDED){
+            action(ref);
+        }
+    });
+}
+
+void Touchable::useTouchMaker(Node* node)
+{
+    auto listener = EventListenerTouchOneByOne::create();
+    listener->onTouchBegan = [](Touch* touch, Event*event){
+        auto particle = ParticleSystemQuad::create("Particles/touch.plist");
+        particle->setPosition(touch->getLocation());
+        event->getCurrentTarget()->addChild(particle);
+        particle->setAutoRemoveOnFinish(true);
+        return true;
+    };
+    auto layer = Layer::create();
+    node->addChild(layer);
+    Director::getInstance()->getEventDispatcher()->addEventListenerWithSceneGraphPriority(listener, layer);
+}
---
+++ Touchable.h
@@ -0,0 +1,39 @@
+//
+//  Touchable.hpp
+//  angel
+//
+//  Created by Atsushi Yoshida on 2015/12/20.
+//
+//
+
+#ifndef Touchable_hpp
+#define Touchable_hpp
+
+#include "cocos2d.h"
+#include "CocosGUI.h"
+
+USING_NS_CC;
+using namespace cocos2d::ui;
+
+class Touchable
+{
+protected:
+    static const std::string LONG_TOUCH_KEY;
+    static const float LONG_TOUCH_SEC;
+    Touchable()
+    : _dt(0.0)
+    {
+    }
+    
+    virtual ~Touchable()
+    {
+    }
+    
+    void onLongTouch(Widget* widget, const std::function<void(Ref* ref)>& action);
+    void onTouch(Widget* widget,  const std::function<void(Ref* ref)>& action);
+    void useTouchMaker(Node* node);
+private:
+    float _dt;
+};
+
+#endif /* Touchable_hpp */
