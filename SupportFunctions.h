a93f059c9dcff197f5d1f2458da24adb7a742e90
yalab <rudeboyjet@gmail.com>
2016-04-02 16:59:22 +0900

    Add SupportFunctions.

---
+++ SupportFunctions.h
@@ -0,0 +1,38 @@
+#ifndef SupportFunctions_h
+#define SupportFunctions_h
+
+#include "cocos2d.h"
+#include "cocos/ui/CocosGUI.h"
+#include <spine/SkeletonAnimation.h>
+
+using namespace cocos2d;
+using namespace cocos2d::ui;
+using namespace spine;
+
+namespace suppoortfunctions{
+    static inline const Vec2&& middleCenter()
+    {
+        auto winSize = Director::getInstance()->getWinSize();
+        return std::forward<Vec2>(Vec2(winSize.width / 2, winSize.height / 2));
+    }
+
+    static inline SkeletonAnimation* createSpineAnimation(const std::string& fname)
+    {
+        return SkeletonAnimation::createWithFile("Animations/" + fname + ".json", "Animations/" + fname + ".atlas");
+    }
+
+    static inline Button* createButton(const std::string& fname)
+    {
+         return Button::create("Images/" + fname + "_n.png", "Images/" + fname + "_p.png", "Images/" + fname + "_d.png");
+    }
+
+    template <typename SceneT, typename TransitionT>
+    static void transition(float duration)
+    {
+        auto scene = SceneT::createScene();
+        auto transition = TransitionT::create(duration, scene);
+        Director::getInstance()->replaceScene(transition);
+    }
+}
+
+#endif /* SupportFunctions_h */
